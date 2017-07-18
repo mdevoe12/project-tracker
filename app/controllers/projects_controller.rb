f9f843de60d119c86acfb5b2ab1bda23c5ffa9e4
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-18 16:00:56 -0600

    Project feature and model tests passing

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path @project
+    else
+      flash[:message] = "Project name already used"
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+end
