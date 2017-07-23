da2b0dcb9a1413684993ae3ab16ec58b226207a9
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-23 14:21:31 -0600

    removed commented out @user lines

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,20 +3,17 @@ class NotesController < ApplicationController
 
   def show
     @project = Project.find(params[:project_id])
-    # @user = User.find(params[:user_id])
     @note = Note.find(params[:id])
 
   end
 
   def new
     @project = Project.find(params[:project_id])
-    # @user = @project.user
     @note = Note.new
   end
 
   def create
     @project = Project.find(params[:project_id])
-    # @user = User.find(params[:user_id])
     @note = @project.notes.new(note_params)
 
     if @note.save
@@ -28,14 +25,12 @@ class NotesController < ApplicationController
   end
 
   def edit
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:project_id])
     @note = Note.find(params[:id])
   end
 
   def update
     @project = Project.find(params[:project_id])
-    # @user = User.find(params[:user_id])
     @note = Note.find(params[:id])
     if @note.update(note_params)
       redirect_to user_project_note_path(current_user, @project, @note)
@@ -46,7 +41,6 @@ class NotesController < ApplicationController
   end
 
   def destroy
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:project_id])
     @note = Note.find(params[:id])
     @note.destroy
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,10 @@ class ProjectsController < ApplicationController
   before_action :check_user
 
   def new
-    # @user = User.find(params[:user_id])
     @project = Project.new
   end
 
   def create
-    # @user = User.find(params[:user_id])
     @project = current_user.projects.new(project_params)
     if @project.save
       redirect_to user_project_path(current_user, @project)
@@ -18,22 +16,18 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:id])
   end
 
   def index
-    # @user = User.find(params[:user_id])
     @projects = Project.all
   end
 
   def edit
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:id])
   end
 
   def update
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:id])
     if @project.update(project_params)
       redirect_to user_project_path(current_user, @project)
@@ -44,7 +38,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    # @user = User.find(params[:user_id])
     @project = Project.find(params[:id])
     @project.destroy
     redirect_to user_projects_path(current_user)
